434dd100bbdc71885ea573eae9a94f41e65587eb
Szzng <seungheeseo53@gmail.com>
2022-11-14 22:51:52 +0900

    basic 코드 추가

---
+++ RPA/PPT/basic.py
@@ -0,0 +1,38 @@
+from pptx import Presentation
+from pptx.util import Cm, Pt
+
+# 프레젠테이션 만들기
+prs = Presentation()
+
+# 슬라이드 레이아웃 종류 확인하기 (0~10)
+for i in range(len(prs.slide_layouts)):
+    print(i, prs.slide_layouts[i].name)
+
+# 첫 번째 슬라이드 만들기 (제목 슬라이드)
+layout = prs.slide_layouts[0]  # 슬라이드 레이아웃 선택
+slide1 = prs.slides.add_slide(layout)  # 선택한 레이아웃으로 슬라이드 추가
+
+shapes = slide1.shapes  # 슬라이드의 도형 가져오기
+shapes[0].text = "첫 번째 슬라이드 제목"  # 첫 번째 도형(제목 도형)에 제목 텍스트 입력
+shapes[1].text = "첫 번째 슬라이드 부제목"  # 두 번째 도형(부제목 도형)에 부제목 텍스트 입력
+
+# 두 번째 슬라이드 만들기 (제목+내용 슬라이드)
+slide2 = prs.slides.add_slide(prs.slide_layouts[1])
+slide2.shapes[0].text = "두 번째 슬라이드 제목"
+slide2.shapes[1].text = "두 번째 슬라이드 내용"
+
+# 세 번째 슬라이드 만들기 (빈 슬라이드 + 텍스트 상자 추가)
+slide3 = prs.slides.add_slide(prs.slide_layouts[6])
+textbox = slide3.shapes.add_textbox(left=Cm(2), top=Cm(2), width=Cm(10), height=Cm(10))
+textbox.text = "세 번째 슬라이드 내용"
+p = textbox.text_frame.add_paragraph()
+p.font.size = Pt(50)
+p.font.bold = True
+p.text = "크고 굵은 글씨의 내용 추가"
+
+# 네 번째 슬라이드 만들기 (빈 슬라이드 + 이미지 추가)
+slide4 = prs.slides.add_slide(prs.slide_layouts[6])
+picture = slide4.shapes.add_picture('룡룡.png', left=Cm(9), top=Cm(6))
+
+# 프레젠테이션 파일을 저장하기
+prs.save("basic.pptx")
