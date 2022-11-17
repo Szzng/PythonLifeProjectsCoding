43989227b3d8d9d46a8f6cc0a14b8a03b0a1a0cd
Szzng <seungheeseo53@gmail.com>
2022-11-17 21:38:55 +0900

    상장 내용도 엑셀 데이터로 변경 / 주석 추가

--- RPA/PPT/makePrizes.py
+++ RPA/PPT/makePrizes.py
@@ -3,7 +3,7 @@ from pptx import Presentation
 from pptx.util import Cm, Pt
 from pptx.enum.text import PP_ALIGN
 
-
+# 슬라이드에 텍스트 추가하는 함수 정의
 def add_text(slide, left, top, width, height, font_size, alignment, text):
     textbox = slide.shapes.add_textbox(left, top, width, height)
     textbox.text_frame.word_wrap = True
@@ -14,23 +14,26 @@ def add_text(slide, left, top, width, height, font_size, alignment, text):
     paragraph.text = text
 
 
+# 엑셀 데이터 가져오기
+data = pd.read_excel('상장목록.xlsx')
+students = data['학생']
+titles = data['제목']
+contents = data['내용']
+
+# 프레젠테이션 만들기
 prs = Presentation()
 full_width = prs.slide_width
 
-data = pd.read_excel('상장목록.xlsx')
-prizes = data['상']
-students = data['학생']
-
 for i in range(len(data)):
+    # 빈 슬라이드 추가
     slide = prs.slides.add_slide(prs.slide_layouts[6])
+    # 상장 배경 이미지 추가
     slide.shapes.add_picture('상장템플릿.png', left=Cm(0), top=Cm(0), width=full_width, height=prs.slide_height)
-
-    # 눈금자 확인
-    add_text(slide, Cm(0), Cm(2), full_width, Cm(0), Pt(50), PP_ALIGN.CENTER, str(prizes[i]))
+    # 상장 내용 추가
+    add_text(slide, Cm(0), Cm(2), full_width, Cm(0), Pt(50), PP_ALIGN.CENTER, str(titles[i]))
     add_text(slide, Cm(0), Cm(5), full_width - Cm(3), Cm(0), Pt(18), PP_ALIGN.RIGHT, '파이썬 초등학교 3학년 10반')
     add_text(slide, Cm(0), Cm(6), full_width - Cm(3), Cm(0), Pt(30), PP_ALIGN.RIGHT, str(students[i]))
-    add_text(slide, Cm(3), Cm(8), full_width - Cm(6), Cm(0), Pt(23), PP_ALIGN.LEFT,
-             '위 학생은 평소 성실하고 책임감 있는 자세로 학교 생활을 하였으며, 친구들을 배려하는 따뜻한 마음을 가지고 있기에 이 상을 수여 합니다.')
+    add_text(slide, Cm(3), Cm(8), full_width - Cm(6), Cm(0), Pt(23), PP_ALIGN.LEFT, str(contents[i]))
     add_text(slide, Cm(0), Cm(13), full_width, Cm(0), Pt(20), PP_ALIGN.CENTER, '2030년 01월 30일')
     add_text(slide, Cm(0), Cm(14.8), full_width, Cm(0), Pt(30), PP_ALIGN.CENTER, '너의 친구 룡룡이')
 
