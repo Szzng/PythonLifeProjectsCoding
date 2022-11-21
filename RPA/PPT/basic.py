a40c249c057e835f3dfdb36dc850a1f916bf4cdc
Szzng <66233687+Szzng@users.noreply.github.com>
2022-11-21 18:23:28 +0900

    원고용 코드 정리

--- RPA/PPT/basic.py
+++ RPA/PPT/basic.py
@@ -5,20 +5,21 @@ from pptx.util import Cm, Pt
 prs = Presentation()
 
 # 슬라이드 레이아웃 종류 확인하기 (0~10)
-for i in range(len(prs.slide_layouts)):
-    print(i, prs.slide_layouts[i].name)
+layouts = prs.slide_layouts
+for i in range(len(layouts)):
+    print(i, layouts[i].name)
 
 # 첫 번째 슬라이드 (제목 슬라이드)
 layout = prs.slide_layouts[0]  # 슬라이드 레이아웃 선택
 slide1 = prs.slides.add_slide(layout)  # 선택한 레이아웃으로 슬라이드 추가
 shapes = slide1.shapes  # 슬라이드의 도형 가져오기
-shapes[0].text = "첫 번째 슬라이드 제목"  # 첫 번째 도형(제목 도형)에 제목 텍스트 입력
-shapes[1].text = "첫 번째 슬라이드 부제목"  # 두 번째 도형(부제목 도형)에 부제목 텍스트 입력
+shapes[0].text = "슬라이드1 제목"  # 첫 번째 도형(제목)에 텍스트 입력
+shapes[1].text = "슬라이드1 부제목"  # 두 번째 도형(부제목)에 텍스트 입력
 
 # 두 번째 슬라이드 (제목 + 내용 슬라이드)
 slide2 = prs.slides.add_slide(prs.slide_layouts[1])
-slide2.shapes[0].text = "두 번째 슬라이드 제목"
-slide2.shapes[1].text = "두 번째 슬라이드 내용"
+slide2.shapes[0].text = "슬라이드2 제목"
+slide2.shapes[1].text = "슬라이드2 내용"
 
 # 세 번째 슬라이드 (빈 슬라이드 + 이미지 추가)
 slide3 = prs.slides.add_slide(prs.slide_layouts[6])
@@ -26,7 +27,7 @@ slide3.shapes.add_picture('룡룡.png', left=Cm(9), top=Cm(6))
 
 # 네 번째 슬라이드 (빈 슬라이드 + 텍스트 상자 추가)
 slide4 = prs.slides.add_slide(prs.slide_layouts[6])
-textbox = slide4.shapes.add_textbox(left=Cm(2), top=Cm(2), width=Cm(15), height=Cm(0)) # 눈금자 확인
+textbox = slide4.shapes.add_textbox(Cm(2), Cm(2), Cm(15), Cm(0))
 textbox.text_frame.word_wrap = True
 paragraph = textbox.text_frame.paragraphs[0]
 paragraph.font.size = Pt(50)
--- RPA/PPT/makePrizes.py
+++ RPA/PPT/makePrizes.py
@@ -3,13 +3,14 @@ from pptx import Presentation
 from pptx.util import Cm, Pt
 from pptx.enum.text import PP_ALIGN
 
+
 # 슬라이드에 텍스트 추가하는 함수 정의
 def add_text(slide, left, top, width, height, font_size, alignment, text):
-    textbox = slide.shapes.add_textbox(left, top, width, height)
+    textbox = slide.shapes.add_textbox(Cm(left), Cm(top), width, Cm(height))
     textbox.text_frame.word_wrap = True
     paragraph = textbox.text_frame.paragraphs[0]
     paragraph.font.bold = True
-    paragraph.font.size = font_size
+    paragraph.font.size = Pt(font_size)
     paragraph.alignment = alignment
     paragraph.text = text
 
@@ -24,17 +25,17 @@ contents = data['내용']
 prs = Presentation()
 full_width = prs.slide_width
 
-for i in range(len(data)):
+for i in range(len(data)):  # data의 개수만큼 반복
     # 빈 슬라이드 추가
     slide = prs.slides.add_slide(prs.slide_layouts[6])
     # 상장 배경 이미지 추가
-    slide.shapes.add_picture('상장템플릿.png', left=Cm(0), top=Cm(0), width=full_width, height=prs.slide_height)
+    slide.shapes.add_picture('상장템플릿.png', Cm(0), Cm(0), full_width, prs.slide_height)
     # 상장 내용 추가
-    add_text(slide, Cm(0), Cm(2), full_width, Cm(0), Pt(50), PP_ALIGN.CENTER, str(titles[i]))
-    add_text(slide, Cm(0), Cm(5), full_width - Cm(3), Cm(0), Pt(18), PP_ALIGN.RIGHT, '파이썬 초등학교 3학년 10반')
-    add_text(slide, Cm(0), Cm(6), full_width - Cm(3), Cm(0), Pt(30), PP_ALIGN.RIGHT, str(students[i]))
-    add_text(slide, Cm(3), Cm(8), full_width - Cm(6), Cm(0), Pt(23), PP_ALIGN.LEFT, str(contents[i]))
-    add_text(slide, Cm(0), Cm(13), full_width, Cm(0), Pt(20), PP_ALIGN.CENTER, '2030년 01월 30일')
-    add_text(slide, Cm(0), Cm(14.8), full_width, Cm(0), Pt(30), PP_ALIGN.CENTER, '너의 친구 룡룡이')
+    add_text(slide, 1, 2, full_width, 0, 50, PP_ALIGN.CENTER, titles[i])
+    add_text(slide, 0, 5, full_width-Cm(3), 0, 18, PP_ALIGN.RIGHT, '파이썬학교 3학년 7반')
+    add_text(slide, 0, 6, full_width-Cm(3), 0, 30, PP_ALIGN.RIGHT, students[i])
+    add_text(slide, 3, 8, full_width-Cm(6), 0, 23, PP_ALIGN.LEFT, contents[i])
+    add_text(slide, 0, 13, full_width, 0, 20, PP_ALIGN.CENTER, '2030년 01월 30일')
+    add_text(slide, 0, 14.8, full_width, 0, 30, PP_ALIGN.CENTER, '너의 친구 룡룡이')
 
 prs.save("result.pptx")
