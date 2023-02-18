f120d5c5f992da9d582e4a1517be322dee4b1e15
Szzng <seungheeseo53@gmail.com>
2023-02-18 12:12:35 +0900

    디지털 시계 만들기 프로젝트 추가

---
+++ 1. 기초편/3. 디지털 시계/코딩지식쌓기/practice_tkinter.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+def print_label():
+    label.configure(text='힘내세요!')
+
+
+window = tk.Tk()  # 창 만들기
+window.title("tkinter 연습")  # 창 제목
+window.geometry('200x100')  # 창 크기
+
+font = ('맑은 고딕', 20, 'bold')
+label = tk.Label(text='라벨', font=font)  # 라벨 만들기
+button = tk.Button(text='클릭!', font=font, command=print_label)  # 버튼 만들기
+
+label.place(relx=0.5, rely=0.3, anchor=tk.CENTER)  # 라벨 위치 지정
+button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)  # 버튼 위치 지정
+window.mainloop()  # 만든 창을 실행하기
---
+++ 1. 기초편/3. 디지털 시계/프로젝트코딩하기/digital_clock.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import time
+
+
+def clock():
+    now = time.strftime("%Y년 %m월 %d일 \n %H:%M:%S")
+    label.configure(text=now)
+    label.after(1000, clock)
+
+
+window = tk.Tk()
+window.title("디지털 시계")
+window.geometry('300x200')
+
+font = ('맑은 고딕', 25, 'bold')
+label = tk.Label(text='시작 버튼을 누르세요', font=font)
+button = tk.Button(text='시작', font=font, width=3, height=1, command=clock)
+
+label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+button.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+window.mainloop()
