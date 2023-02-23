3f4acd123f9f7cbd1982fe23c2ef67f008fa5fe7
Szzng <66233687+Szzng@users.noreply.github.com>
2023-02-23 19:30:59 +0900

    코딩지식쌓기 코드 추가

--- 1. 기초편/1. 터틀 미술/1.코딩지식쌓기_turtle.py
+++ 1. 기초편/1. 터틀 미술/1.코딩지식쌓기_turtle.py
@@ -1,9 +1,17 @@
-import turtle as t
+# 터틀 기본 명령어 익히기
+# import turtle as t
+# t.shape('turtle')
+# t.fd(100)
+# t.rt(90)
+# t.fd(100)
+# t.lt(60)
+# t.fd(100)
 
+# 터틀에게 반복 명령어 내리기
+import turtle as t
 t.shape('turtle')
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
-
 for c in colors:
     t.color(c)
-    t.fd(100)
+    t.fd(200)
     t.lt(144)
