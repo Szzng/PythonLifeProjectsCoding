726f769f1c09e04540b3c31bb812433d5d7ebaa0
Szzng <66233687+Szzng@users.noreply.github.com>
2023-03-07 19:03:49 +0900

    주석 수정

--- 1. 기초편/2. 터틀 그림판/2.프로젝트코딩_터틀그림판.py
+++ 1. 기초편/2. 터틀 그림판/2.프로젝트코딩_터틀그림판.py
@@ -7,9 +7,9 @@ t.onkeypress(go_right, 'Right')
 t.onkeypress(go_up, 'Up')
 t.onkeypress(go_left, 'Left')
 t.onkeypress(go_down, 'Down')
-t.onkeypress(pen_updown, 'Return')
-t.onkeypress(change_color, 'c')  # c 대소문자 구분
-t.onkeypress(clear, 'Escape')  # esc key
+t.onkeypress(pen_updown, 'Return') # Enter 키
+t.onkeypress(change_color, 'c')  # c 대소문자 구분해야 함
+t.onkeypress(clear, 'Escape')  # esc 키
 
 t.listen()
 t.mainloop()
