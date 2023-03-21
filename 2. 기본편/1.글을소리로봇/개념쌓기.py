e6a83eed31a21e61a75a0f6e4272985b4b0b5d5c
Szzng <66233687+Szzng@users.noreply.github.com>
2023-03-21 17:55:00 +0900

    글을소리로봇 코드 정리

--- 2. 기본편/1.글을소리로봇/개념쌓기.py
+++ 2. 기본편/1.글을소리로봇/개념쌓기.py
@@ -4,6 +4,6 @@ import time
 
 text = "안녕하세요, 횡단보도에 녹색불이 켜졌습니다. 건너가도 좋습니다."
 tts = gTTS(text=text, lang='ko')
-tts.save('traffic_light.mp3')
-time.sleep(1)
-playsound('traffic_light.mp3')
+tts.save('trafficlight.mp3')
+time.sleep(5)
+playsound('trafficlight.mp3')
--- 2. 기본편/1.글을소리로봇/프로젝트.py
+++ 2. 기본편/1.글을소리로봇/프로젝트.py
@@ -3,11 +3,9 @@ from playsound import playsound
 import time
 
 lang = input('언어 선택 - 영어 또는 한국어를 입력해주세요. :')
-condition = '영어' in lang or '한국어' in lang
 
-while not condition:
+while '영어' not in lang and '한국어' not in lang:
     lang = input('잘못 입력하셨습니다. 언어 선택 - 영어 또는 한국어를 입력해주세요. :')
-    condition = '영어' in lang or '한국어' in lang
 
 if '영어' in lang:
     text = input("영어 문장을 입력하세요. : ")
@@ -17,6 +15,6 @@ else:
     text = input("한국어 문장을 입력하세요. : ")
     tts = gTTS(text=text, lang='ko')
 
-tts.save('read_text.mp3')
-time.sleep(3)
-playsound('read_text.mp3')
+tts.save('speech.mp3')
+time.sleep(5)
+playsound('speech.mp3')
