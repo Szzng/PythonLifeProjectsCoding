3963adfaa996b24d23332fe89a1be33c4c7ed6ef
Szzng <66233687+Szzng@users.noreply.github.com>
2023-04-14 16:21:57 +0900

    clean code

--- 2. 기본편/6. 나의 날씨 비서/2.프로젝트코딩_나의날씨비서.py
+++ 2. 기본편/6. 나의 날씨 비서/2.프로젝트코딩_나의날씨비서.py
@@ -9,7 +9,6 @@ def create_soup(url):
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/86.0.4240.183 Safari/537.36"}
     res = requests.get(url, headers=headers)
-    res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
     return soup
 
