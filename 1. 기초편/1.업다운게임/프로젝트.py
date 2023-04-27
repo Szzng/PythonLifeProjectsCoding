c4ac70d814f30b8b04ddeac672aae16aa645c7ad
Szzng <seungheeseo53@gmail.com>
2023-04-27 17:57:35 +0900

    도전 횟수 추가

--- 1. 기초편/1.업다운게임/프로젝트.py
+++ 1. 기초편/1.업다운게임/프로젝트.py
@@ -1,12 +1,15 @@
 import random
 
 number = random.randint(1, 100)
+count = 0
 
 while True:
     answer = int(input('내가 고른 숫자를 맞춰 봐!'))
+    count += 1
 
     if answer == number:
         print('제법이군! 정답이야!')
+        print(str(count)+'번 만에 정답을 맞혔어!')
         break
 
     elif answer < number:
